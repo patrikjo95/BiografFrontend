31384c46d6ecefea4a9aa15d1cce167c577774be
Patrik <patrikjo95@gmail.com>
2022-03-25 11:09:33 +0100

    Initial commit

---
+++ src/main/java/com/example/biograffrontend2/Application.java
@@ -0,0 +1,34 @@
+package com.example.biograffrontend2;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class Application extends javafx.application.Application {
+    private static Stage newStage;
+
+    public Application() {
+    }
+
+    public void start(Stage stage) throws IOException {
+        newStage = stage;
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("MovieMenu.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void changeScene(String fxml) throws IOException {
+        Parent pane = FXMLLoader.load(Objects.requireNonNull(this.getClass().getResource(fxml)));
+        newStage.getScene().setRoot(pane);
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/example/biograffrontend2/Controller.java
@@ -0,0 +1,48 @@
+package com.example.biograffrontend2;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
+public class Controller {
+    @FXML
+    private TextField passwordField;
+    @FXML
+    private TextField userNameField;
+    @FXML
+    private Label loginErrorLabel;
+
+    public Controller() {
+    }
+
+    @FXML
+    protected void menuItemCloseClicked() {
+        System.exit(0);
+    }
+
+    @FXML
+    protected void bokaButtonClicked() throws IOException {
+    }
+
+    @FXML
+    protected void adminButtonClicked(ActionEvent event) throws IOException {
+        Application a = new Application();
+        a.changeScene("adminLogin.fxml");
+
+    }
+
+    @FXML
+    protected void loginButtonClicked(ActionEvent event) throws IOException {
+        Application m = new Application();
+        if (this.userNameField.getText().equals("sexy") && this.passwordField.getText().equals("1234")) {
+            m.changeScene("schema.fxml");
+        } else {
+            this.loginErrorLabel.setText("Puder dont accept your request ");
+            this.loginErrorLabel.setOpacity(1);
+        }
+
+    }
+}
